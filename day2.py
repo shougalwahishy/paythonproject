4f1d51b9cb907aa2c735e92c65ab766c41324c86
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-24 00:19:39 +0300

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,7 +1,11 @@
 print ("hello,guys!") #This is comment
-print ("I love paython!") 
+print ("I love python!") 
 """ Paython is the best language in the world """
 print ("Hi programers")
 #Be patient
 #Be positive
 #Be flexible
+
+hello,guys!
+I love python!
+Hi programers
