806a052ad0e77647820a6d2f51b1ada72e1b0924
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-24 00:14:42 +0300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,7 @@
+print ("hello,guys!") #This is comment
+print ("I love paython!") 
+""" Paython is the best language in the world """
+print ("Hi programers")
+#Be patient
+#Be positive
+#Be flexible
