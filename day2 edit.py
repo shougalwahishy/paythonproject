bbfe9ae5e86be2d54138555b58c3dbac114b49e7
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-24 00:00:25 +0300

    Add files via upload

---
+++ day2 edit.py
@@ -0,0 +1,7 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\DELL\Desktop\100days-code\day2.py ============
+hello guys!
+I love paython!
+>>> 
