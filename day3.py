b6e901a7696de95f8e752800106db47eb12403e3
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-24 00:43:58 +0300

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,18 @@
+x = 3 # x is of type int
+x = "Shoug" # x is now of type str
+print (x) 
+x , y , z = "Banana" , "cherry" , "Apple"
+print (x)
+print (y)
+print (z)
+x = y = z ="kiwi"
+print (x)
+print (y)
+print (z)
+x = " awesome "
+print ("python is" +  x)
+x = 4    y = 3 
+ print=(x+y)
+ x=6 
+ y="Shoug"
+ print(x+y)
