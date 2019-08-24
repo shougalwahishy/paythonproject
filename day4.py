79dc7ed5e0d9b3c77d03c08f0471d5bc3110021a
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-24 23:14:53 +0300

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,35 @@
+x = 1
+y = 6543345
+z = -8765
+print (type(x))
+print(type(y))
+print(type(z))
+x = 1.10
+y = 5j
+z = -8765
+print (type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1 #int
+y = 2.8 #float
+z = 1J #complex
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex (x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+print(random.randrange(1,10))
+
