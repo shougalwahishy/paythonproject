d8b5627279e6b3deffa338f0c099ce263f5fd5f5
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-27 23:34:49 +0300

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,12 @@
+a = "Hello, world!"
+print (a.strip()) #returns "Hello, world!"
+a = "Hello, world!"
+print (len(a))
+a = "Hello, World!"
+print (a.lower())
+a = "Hello, World!"
+print (a.upper())
+a = "Hello, World!"
+print (a.replace("H","J"))
+a = "Hello, World!"
+print(a.split(",")) #returns ['Hello', 'world!']
