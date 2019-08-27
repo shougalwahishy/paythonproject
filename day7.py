0690ff9773b96c9219bf9fa4676c35738ed97fe3
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-27 23:18:15 +0300

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,14 @@
+#week2
+#day7
+print("hello")
+print('hello')
+
+a=''' Python is a general purpose programming language. 
+Hence, you can use the programming language for developing both
+ desktop and web applications'''
+print(a)
+
+a = "Hello,world!"
+print (a[1])
+b = "Hello ,world!"
+print (b[2:5])
