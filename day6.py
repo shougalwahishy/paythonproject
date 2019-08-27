3125af54f05c64a13deb2d065f469657ec974057
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-27 22:54:02 +0300

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,22 @@
+x = int (1) # x will be 1
+y = int (2.8) # y will be 2
+z = int ("3") # z will be 3
+w = float("4.2") # w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+x = float(1) # x will be 1
+y = float (2.8) # y will be 2
+z = float("3") # z will be 3
+w = float("4.2") # w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+x = str ("sl") # x will be 'sl'
+y = str(2) # y will be '2'
+z = (3.0) # z will be '3.0'
+print(x)
+print(y)
+print(z)
