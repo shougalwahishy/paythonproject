077dd856ef960f014ac2d831c8218a95c4a8c4a4
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-30 22:57:14 +0300

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,15 @@
+
+age = 25
+txt ="My name is shoug,and I am {}"
+print(txt.format(age))
+quantity = 3
+itemno = 567
+price = 49.95
+myorder ="I want {} pieces of item {} for {} dollars"
+print(myorder.format(quantity,itemno,price))
+quantity = 5
+itemno = 565
+price = 50.20
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity,itemno,price))
+
