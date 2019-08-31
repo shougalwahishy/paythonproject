56833773f05a46f1342213133d152f437d6013d8
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-08-31 22:53:17 +0300

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,8 @@
+a = "please,I want {} sandwishes and {} donuts"
+print(a.replace("I","We"))
+quantity=5
+quantity2=7
+myorder="please,I want {} sandwishes and {} donuts"
+print(myorder.format(quantity,quantity2))
+a ="please,I want {} sandwishes and {} donuts"
+print(a.replace("a","A"))
