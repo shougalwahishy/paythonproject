a8d2676a4fc4f0fffac17c83ef7f4343d2c22ba6
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-01 23:38:10 +0300

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,22 @@
+#WEEK3
+#DAY13
+numbers = [1,2,3,4,5]
+print(numbers)
+thelist = ["apple","banana","cherry"]
+print (thelist)
+thelist = ["apple","banana","cherry",1,2,3]
+print(thelist)
+thelist = ["apple","banana","cherry"]
+for x in thelist :
+    print (x)
+thelist = ["apple","banana","cherry"]
+print (thelist[1])
+thelist = ["apple","banana","cherry"]
+thelist[1]="kiwi"
+print(thelist)
+thelist = ["apple","banana","cherry"]
+del thelist[0]
+print(thelist)
+numberslist = [1.1, 1.5, 2.1,2.5 ,3.1]
+for x in numberslist:
+    print (x)
