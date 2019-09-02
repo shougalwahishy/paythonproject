7728d31c8bded9c4e5d69d74f242fa82927c0f34
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-02 22:13:10 +0300

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,11 @@
+thelist = ["A","B","C","D","E"]
+print (thelist[1:3])
+thelist = ["apple","banana","cherry"]
+if "apple" in thelist:
+    print("yes,'apple' is in the fruits list")
+    thelist=["python"]*5
+    print(thelist)
+    thelist1 =["Jomanh","Nouf","Rawan"]
+    thelist2 =["wejdan","Fatoom"]
+    thelist3 =thelist1 + thelist2
+    print(thelist3)
