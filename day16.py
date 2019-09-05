d9d7b4175d919cef7db7a2ad7056d996e805f6c8
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-05 23:42:05 +0300

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,21 @@
+#week2
+#day16
+thistuple = ()
+print(thistuple)
+thistuple = (4,)
+print(thistuple)
+thistuple = (2,5.9,7,8)
+print(thistuple)
+thistuple =("Ahmad",1.1,4,"python")
+print(thistuple)
+thistuple = ("apple","banana","cherry")
+print(thistuple[1])
+thistuple = ("apple","cherry","kiwi")
+for x in thistuple:
+    print(x)
+   thistuple = ("apple","banana","cherry")
+   del thistuple
+   print(thistuple)
+   thistuple = ("apple","banana","orange","cherry")
+   print(thistuple[0:3])
+   
