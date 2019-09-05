fd8724b790ff176de9daf34db8caba783d86184a
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-05 23:20:11 +0300

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,25 @@
+thelist = ["apple","banana","cherry"]
+print(len(thelist))
+thelist = ["apple","banana","cherry"]
+thelist.append("orange")
+print(thelist)
+thelist = ["apple","banana","cherry"]
+thelist.insert(1,"orange")
+print(thelist)
+thelist =["apple","kiwi","cherry"]
+thelist.remove("kiwi")
+print(thelist)
+thelist =["kiwi","apple","orange","cherry"]
+thelist.pop(3)
+print(thelist)
+thelist =["apple","kiwi","cherry"]
+thelist.clear()
+print(thelist)
+thelist =["apple","kiwi","cherry"]
+mylist = thelist.copy()
+print(mylist)
+thelist =["Ahmad","omar","Khalid"]
+mylist = thelist.copy()
+thelist.pop(0)
+print(mylist)
+print(thelist)
