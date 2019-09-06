4048a9c96fe8811d3c5cfa3595a13269d22d2274
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-06 22:50:52 +0300

    Add files via upload

---
+++ day18,19.py
@@ -0,0 +1,17 @@
+#the first:
+thelist =["Bedoor","JOJO","Reemz","SHOSHO"]     
+# 1
+thelist.remove("JOJO")       
+print(thelist)
+thelist =["Bedoor","JOJO","Reemz","SHOSHO"]
+# 2
+thelist.pop(3)
+print(thelist)
+thelist =["Bedoor","JOJO","Reemz","SHOSHO"]
+# 3
+thelist.clear()
+print(thelist)
+thelist = ["Bedoor","JOJO","Reemz","SHOSHO"]
+thelist.insert(4,"Wateen")
+# 4
+print(thelist)
