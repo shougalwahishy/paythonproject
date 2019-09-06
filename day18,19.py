637be4dfd1cb80a1647bf1d22f2041148f828b57
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-06 22:54:45 +0300

    Add files via upload

---
+++ day18,19.py
@@ -0,0 +1,21 @@
+#the first:
+thelist =["Bedoor","JOJO","Reemz","SHOSHO"]     
+# 1
+thelist.remove("JOJO")       
+print(thelist)
+thelist =["Bedoor","JOJO","Reemz","SHOSHO"]
+# 2
+thelist.pop(3)
+print(thelist)
+thelist =["Bedoor","JOJO","Reemz","SHOSHO"]
+# 3
+thelist.clear()
+print(thelist)
+thelist = ["Bedoor","JOJO","Reemz","SHOSHO"]
+thelist.insert(4,"Wateen")
+# 4
+print(thelist)
+# second:
+tuple = ('java','python','swift')
+if 'python' in tuple:
+ print('python is in our tuple')
