6f7d2ee256a3eb2c16c5574ec76b71259bc5a229
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-06 22:01:36 +0300

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,13 @@
+thistuple = ("apple","cherry","banana")
+if "apple" in thistuple:
+    print("yes,'apple' is in the fruits tuple")
+    thistuple = ("python,")*3
+    print(thistuple)
+    x = (3,4,5,6)
+    x = x +(1,2,3)
+    print(x)
+    thistuple = ("apple","banana","cherry")
+    print(len(thistuple))
+    thislist = [3,4,5,6,"A","B"]
+    thistuple = tuple(thislist)
+    print(thistuple)
