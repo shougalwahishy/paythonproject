bf0a15032d2200e1149604f5f6a5b9a55addc23c
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-14 23:14:08 +0300

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,16 @@
+
+# python sets
+# week4
+thisset ={"apple","banana","cherry"}
+print(thisset)
+thisset ={"shoug","shoug",1,2,3,5}
+print(thisset)
+thisset = {"apple","banana","kiwi","cherry"}
+for x in thisset:
+ print(x)
+ thisset ={"apple","banana","cherry"}
+ thisset.add("orange")
+print(thisset)
+ u = {"apple","banana"}
+  u.update(["kiwi","orange","mango"])
+  print(u)
