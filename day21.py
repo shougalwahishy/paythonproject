447fa0c02c27a1f26aa3ae8be6386d0761472e67
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-14 23:14:30 +0300

    Add files via upload

---
+++ day21.py
@@ -0,0 +1,12 @@
+# data strucure
+thisset = {"she","is","samrt"} 
+print(len(thisset))
+thisset.remove("is")
+say = {"apple","banana","shosho","welcome"}
+x = say.pop()
+print(x)
+print(say)
+say.clear()
+print(say)
+i = (("hello","hello","hello"))
+print(i)
