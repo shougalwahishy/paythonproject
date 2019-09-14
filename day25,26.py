1d84e72a93ce10fd6bd98e971b538567dffa6cd9
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-14 23:40:45 +0300

    Add files via upload

---
+++ day25,26.py
@@ -0,0 +1,33 @@
+numset = {1,2,3,4}
+
+numset.update([5,6,7,8])
+
+print(numset)
+
+numset.remove(8)
+
+print(numset)
+
+numset.clear()
+
+print(numset)
+
+thisdict ={
+
+
+
+"name":"pigeion",
+
+"type":"bird",
+
+"skin cover" :"feathers"
+
+}
+
+x = thisdict["type"]
+
+print(x)
+
+thisdict["skin cover"] = "fur"
+
+print(thisdict)
