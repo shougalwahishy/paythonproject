13fc27f1ca65fed6e910ee97d1c07a77590ab26b
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-19 23:28:14 +0300

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,21 @@
+
+
+def my_function():
+    print("hello from a function")
+
+    my_function
+
+def my_function(fname):
+    print (fname + "refsnes")
+
+    my_function("emil")
+    my_function("tobias")
+    my_function ("linus")
+
+    def my_function(country = "ksa"):
+    print ("i am from"+ country)
+
+    my_function("norway")
+    my_function("switzerland")
+    my_function ("italy")
+    my_function()
