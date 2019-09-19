551410b53d3f086dd04d74959588d423bfa53f2a
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-19 22:56:34 +0300

    Add files via upload

---
+++ day28.py
@@ -0,0 +1,28 @@
+i = 1
+while  i < 6:
+    print (i)
+    i +=1
+
+
+    i = 1
+while  i < 6:
+    print (i)
+    if i == 3:
+        break 
+    i += 1
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print (i) 
+
+
+i = 1
+while  i < 6:
+    print (i)
+    i +=1
+    else:
+        print ("i is no longer less than 6")
+        
