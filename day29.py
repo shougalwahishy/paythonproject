c269d9adb4ef9e782125c07f9bb50dced68d9e6c
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-19 23:04:25 +0300

    Add files via upload

---
+++ day29.py
@@ -0,0 +1,31 @@
+fruits = ["apple","banana","cherry"]
+
+for x in fruits:
+
+    print(x)
+
+famliy = ["zooz","shosho","nood"]
+
+for x in "shosho":
+
+  print(x)
+
+  nepghow = ["medo","wareef","qusai"]
+
+for x in nepghow :
+
+  if x == "medo": 
+
+   break   
+
+print(x)
+
+h = ["moha","hanoo","jojo"]
+
+for x in h:
+
+    if x == "hanoo":
+
+        continue
+
+    print(x)
