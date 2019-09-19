e57bfeef8043ae39db6deca8add5ab1e7c3c676b
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-19 22:43:54 +0300

    Add files via upload

---
+++ day27.py
@@ -0,0 +1,45 @@
+a =33
+b = 200
+if b> a:
+    print("b is greater than a ")
+
+
+a = 44
+b = 44
+if b > a:
+    print ("b is greater than a")
+    elif a == b:
+        print ("a and b are equal") 
+
+  a = 200
+   b = 33
+if b > a:
+    print ("b is greater than a")
+    elif a == b:
+        print ("a and b are equal")
+
+        a = 2
+        b = 330
+        print("A") if a > b else print ("B")
+
+        a = 200
+         b = 33
+         c = 500
+         if a > b and c > a:
+             print("both conditions are true")
+
+           a = 220
+           b = 33
+           c = 500
+        if a > b or a > c:
+            print("At least one of the comditions is true")
+
+            x = 41
+            if x > 10:
+                print ("Above ten,")
+                if x > 20:
+                print ("and also above 20"!)
+                else:
+                    print ("but not above 20.")
+                    
+
