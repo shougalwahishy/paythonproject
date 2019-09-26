24065016f6fcc2da17d4dd88e8699ed32068ea43
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-27 00:15:15 +0300

    Add files via upload

---
+++ day34.py
@@ -0,0 +1,29 @@
+def my_function(food):
+
+for x in food :
+print(x)
+
+fruits = ["apple","banana","cherry"]
+my_function(fruits)
+
+def my_function(x) :
+
+return 5 * x 
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+def my_function(*kids)
+print("the youngest child is " +kids[2])
+my_function("emil","tobias","linus")
+
+def tri_recursinon(k):
+    if (k>0):
+        result = k+tri_recursinon(k-1)
+        print(result)
+        else:
+            result = 0
+            return result
+            print ("\n\nRecursion Example Results")
+            tri_recursion(6)
