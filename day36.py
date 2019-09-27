4f9df1d05d8209f515724712e0a18984d9c9d4de
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-27 23:54:55 +0300

    Add files via upload

---
+++ day36.py
@@ -0,0 +1,14 @@
+def myfunc(n):
+    return lambda a : a * n
+    mydoubler = myfunc(2)
+    print (mydoubler(11))
+
+    def myfunc (n):
+        return lambda a : a * n
+
+        mydoubler = myfunc(2)
+        mytripler = myfunc(3)
+
+        print(mydoubler(11))
+        print(mytripler(11))
+        
