ac9916191f235de6b4385d87c289651a4f997535
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-28 00:04:43 +0300

    Add files via upload

---
+++ day37.py
@@ -0,0 +1,15 @@
+cars = ["ford","volvo","bmw"]
+
+x = cars[0]
+
+print(x)
+
+cars = ["ford","volvo","bmw"]
+cars[0] = "toyota"
+
+print(cars)
+
+cars = ["ford","volvo","bmw"]
+
+x= len (cars)
+print (x)
