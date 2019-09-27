8c40402366c6bfa4cac7c9f5bbd907d0dfe34081
shougalwahishy <47864667+shougalwahishy@users.noreply.github.com>
2019-09-28 00:05:33 +0300

    Add files via upload

---
+++ day38.py
@@ -0,0 +1,15 @@
+cars = ["ford","volvo","bmw"]
+for x in cars :
+    print(x)
+
+    cars = ["ford","volvo","bmw"]
+    cars.append("Honda")
+    print(cars)
+
+cars = ["ford","volvo","bmw"]
+cars.pop(1)
+print(cars)
+
+cars = ["ford","volvo","bmw"]
+cars.remove("Volvo")
+print(cars)
